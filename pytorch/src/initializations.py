fb0f6cfb540159a1dc09e91fc51e2f0be66076da
Kaitlin Maile <kaitlinmaile@gmail.com>
2022-12-09 07:18:37 +0100

    streamline

--- pytorch/src/initializations.py
+++ pytorch/src/initializations.py
@@ -1,10 +1,10 @@
 import torch
-import torch.nn
+import torch.nn as nn
 
 def kaiming_uniform(tensor: torch.Tensor, a: float = 0, mode: str = 'fan_in', nonlinearity: str = 'leaky_relu', fan: int = -1):
     if fan == -1:
-        fan = torch.nn.init._calculate_correct_fan(tensor, mode)
-    gain = torch.nn.init.calculate_gain(nonlinearity, a)
+        fan = nn.init._calculate_correct_fan(tensor, mode)
+    gain = nn.init.calculate_gain(nonlinearity, a)
     std = gain / torch.sqrt(fan)
     bound = torch.sqrt(3.0) * std  
     with torch.no_grad():
