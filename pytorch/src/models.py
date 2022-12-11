4bf4afe185b39b8858120955d4e0af9000a29a1f
Kaitlin Maile <kaitlinmaile@gmail.com>
2022-12-11 23:59:13 +0100

    imports

--- pytorch/src/models.py
+++ pytorch/src/models.py
@@ -13,14 +13,15 @@ class ModSequential(nn.Sequential):
         if self.trackacts:
             self.activations = defaultdict(torch.Tensor)
             for name, module in self.named_modules():
-                if isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d):
+                if isinstance(module, ModLinear) or isinstance(module, ModConv2d):
                     module.register_forward_hook(partial(self._hook, name))
 
     def _hook(self, name, module, input, output):
         self.activations[name] = torch.cat((self.activations[name], output.detach()), dim=0)
         if self.activations[name].shape[0] > 2*self.activations[name].shape[1]:
+            #self.activations[name] = self.activations[name][min(-2*self.activations[name].shape[1], output.shape[1]):]
             self.activations[name] = self.activations[name][-2*self.activations[name].shape[1]:]
-        
+            
     def forward(self, x):
         for module in self._modules.values():
             x = module(x)
@@ -35,9 +36,9 @@ class ModSequential(nn.Sequential):
 
     def mask(self, layerindex: int, neurons: list = [], clearacts: bool = False):
         for i, module in enumerate(self._modules.values()):
-            if i == layerindex and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            if i == layerindex and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.mask(neurons, [])
-            elif i == layerindex+1 and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            elif i == layerindex+1 and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.mask([], neurons)
         if clearacts and self.trackacts:
             for index in (layerindex+1, layerindex):
@@ -46,9 +47,9 @@ class ModSequential(nn.Sequential):
     
     def unmask(self, layerindex: int, neurons: list = [], clearacts: bool = False):
         for i, module in enumerate(self._modules.values()):
-            if i == layerindex and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            if i == layerindex and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.mask(neurons, [])
-            elif i == layerindex+1 and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            elif i == layerindex+1 and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.mask([], neurons)
         if clearacts and self.trackacts:
             for index in (layerindex+1, layerindex):
@@ -57,9 +58,9 @@ class ModSequential(nn.Sequential):
 
     def prune(self, layerindex: int, neurons: list = [], optimizer=None, clearacts: bool = False):
         for i, module in enumerate(self._modules.values()):
-            if i == layerindex and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            if i == layerindex and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.prune(neurons, [], optimizer=optimizer)
-            elif i == layerindex+1 and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            elif i == layerindex+1 and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.prune([], neurons, optimizer=optimizer)
         for index in (layerindex+1, layerindex):
             if clearacts and self.trackacts:
@@ -73,9 +74,9 @@ class ModSequential(nn.Sequential):
 
     def grow(self, layerindex: int, newneurons=0, faninweights=None, fanoutweights=None, optimizer=None, clearacts: bool = False):
         for i, module in enumerate(self._modules.values()):
-            if i == layerindex and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            if i == layerindex and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.grow(newneurons, 0, faninweights, optimizer=optimizer)
-            elif i == layerindex+1 and (isinstance(module, layers.ModLinear) or isinstance(module, layers.ModConv2d)):
+            elif i == layerindex+1 and (isinstance(module, ModLinear) or isinstance(module, ModConv2d)):
                 module.grow(0, newneurons, fanoutweights, optimizer=optimizer)
         for index in (layerindex+1, layerindex):
             if clearacts and self.trackacts:
