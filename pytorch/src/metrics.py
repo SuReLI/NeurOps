8cf287c7b4d4acba300ac41a48d1d0c7dc10f198
Kaitlin Maile <kaitlinmaile@gmail.com>
2022-12-12 00:19:27 +0100

    scale

--- pytorch/src/metrics.py
+++ pytorch/src/metrics.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 """
 Measure effective rank via thresholding singular values of activations (or weights)
 """
-def effectivesvd(tensor: torch.Tensor = None, threshold: float = 0.01, partial: bool = False):
+def effectivesvd(tensor: torch.Tensor = None, threshold: float = 0.01, partial: bool = False, scale: bool = True):
     if tensor is None:
         return None
     if len(tensor.shape) > 2:
         tensor = tensor.reshape(tensor.shape[0], -1) #TODO: check if this is correct for acts and weights
+    if scale:
+        tensor /= tensor.shape[1]**0.5
     _, S, _ = torch.svd(tensor, compute_uv=False)
     effdim = torch.count_nonzero(S > threshold)
     if partial:
@@ -18,7 +20,7 @@ def effectivesvd(tensor: torch.Tensor = None, threshold: float = 0.01, partial: 
 """
 Measure effective rank when each neuron is left out of the computation
 """
-def svdscore(tensor: torch.Tensor = None, threshold: float = 0.01, addwhole: bool = False):
+def svdscore(tensor: torch.Tensor = None, threshold: float = 0.01, addwhole: bool = False, scale: bool = True):
     if tensor is None:
         return None
     scores = torch.zeros(tensor.shape[1])
@@ -26,6 +28,8 @@ def svdscore(tensor: torch.Tensor = None, threshold: float = 0.01, addwhole: boo
         prunedtensor = torch.cat((tensor[:, :neuron], tensor[:, neuron+1:]), dim=1)
         if len(prunedtensor.shape) > 2:
             prunedtensor = prunedtensor.reshape(tensor.shape[0], -1)
+        if scale:
+            prunedtensor /= prunedtensor.shape[1]**0.5
         _, S, _ = torch.svd(prunedtensor, compute_uv=False)
         effdim = torch.sum(S)
         if addwhole:
