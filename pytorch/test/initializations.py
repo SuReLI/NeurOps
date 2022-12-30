85cb286be2c7268170ca929f6a8c03308664d7cf
Kaitlin Maile <kaitlinmaile@gmail.com>
2022-12-30 17:38:45 +0100

    test initalizations for conv

--- pytorch/test/initializations.py
+++ pytorch/test/initializations.py
@@ -31,6 +31,29 @@ class TestInitializations(unittest.TestCase):
         self.assertTrue(torch.allclose(original2, model[2].weight.data[:,:4]))
         self.assertFalse(torch.allclose(model[2].weight.data[:,4:], torch.zeros([4,2])))
 
+        model = ModSequential(
+            ModConv2d(in_channels=1, out_channels=8, kernel_size=3, masked=True),
+            ModConv2d(in_channels=8, out_channels=1, kernel_size=3, masked=True),
+            track_activations=True
+        )
+        x = torch.randn(10, 1, 5, 5)
+        H = x
+        for layer in model:
+            original = layer.weight.data
+            layer.weight.data = iterative_orthogonalization(layer.weight.data, H)
+            self.assertFalse(torch.allclose(original, layer.weight.data))
+            H = layer(H)
+        original1 = model[0].weight.data
+        original2 = model[1].weight.data
+        model.grow(0,2,"iterative_orthogonalization", None, send_activations = True)
+        self.assertTrue(model[0].weight.size(0) == 10)
+        self.assertTrue(torch.allclose(original1, model[0].weight.data[:8]))
+        self.assertFalse(torch.allclose(model[0].weight.data[8:], torch.zeros([2,8,3,3])))
+        self.assertTrue(model[1].weight.size(1) == 10)
+        self.assertTrue(torch.allclose(original2, model[1].weight.data[:,:8]))
+        self.assertTrue(torch.allclose(model[1].weight.data[:,8:], torch.zeros([1,2,3,3])))
+
+
     def test_kaiming_uniform(self):
         model = ModSequential(
             ModLinear(5, 6, masked=True),
@@ -42,7 +65,6 @@ class TestInitializations(unittest.TestCase):
             original = layer.weight.data.clone()
             layer.weight.data = kaiming_uniform(layer.weight.data)
             self.assertFalse(torch.allclose(original, layer.weight.data))
-            self.assertTrue(torch.allclose(original.mean(), layer.weight.data.mean(), atol=1e-1))
             self.assertTrue(torch.allclose(original.std(), layer.weight.data.std(), atol=1e-1))
         original1 = model[1].weight.data
         original2 = model[2].weight.data
